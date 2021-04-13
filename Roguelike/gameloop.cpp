9df153029dc57163cb731c9ce32eb3d58635e613
Patryk <patrykyrtap@o2.pl>
2021-04-13 22:19:34 +0200

    Add sf::View

--- Roguelike/gameloop.cpp
+++ Roguelike/gameloop.cpp
@@ -9,18 +9,28 @@
 #include "constants.hpp"
 
 void gameloop() {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "sfml plswrk");
-    window.setVerticalSyncEnabled(true);
 
-    sf::View view(sf::Vector2f(100.f, 50.f), sf::Vector2f(SCREEN_WIDTH, SCREEN_HEIGHT));
-    window.setView(view);
-    entity::tests::korwintest test(utils::Position(100, 100));
-    auto korwinRenderer = test.GetComponent<component::Renderable>();
+    // ========================== GAME WINDOW ========================== 
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "sfml plswrk");
+    sf::View view;
+    view.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    view.setCenter(100.f, 100.f);
+
+    int viewCounter{};
+    
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(60);
+
+    //entity::tests::korwintest test(utils::Position(100, 100));
+    //auto korwinRenderer = test.GetComponent<component::Renderable>();
 
     sf::RectangleShape s(sf::Vector2f(50.f, 50.f));
 
+
+    // ========================== GAME LOOP ========================== 
     while (window.isOpen()) {
 
+        // EVENTS
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) 
@@ -29,14 +39,33 @@ void gameloop() {
                 window.close();
         }
 
+        // Simple movement mechanics
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            s.move(sf::Vector2f(50, 0.f));
-        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-        //    view.zoom(0.5f);
+            s.move(sf::Vector2f(5, 0.f));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            s.move(sf::Vector2f(-5, 0.f));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            s.move(sf::Vector2f(0, -5.f));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            s.move(sf::Vector2f(0, 5.f));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+            view.rotate(15.f);
+
+        if (viewCounter >= 15) {
+            viewCounter = 0;
+            view.move(sf::Vector2f(-5.f, 0.f));
+        }
+        else {
+            viewCounter++;
+        }
  
+        // Refresh view
+        window.setView(view);
+
+        // DRAWING SECTION
         window.clear();
-        //indow.draw(s);
-        window.draw(*(korwinRenderer->Render()));
+        window.draw(s);
+        //window.draw(*(korwinRenderer->Render()));
 
         window.display();
     }
--- Roguelike/renderable.cpp
+++ Roguelike/renderable.cpp
@@ -2,6 +2,7 @@
 
 namespace component {
     Renderable::Renderable(std::shared_ptr<entity::Entity> parent_) :component::Component(parent_) {}
+    
     sf::Sprite* Renderable::Render() {
         return nullptr;
     }
